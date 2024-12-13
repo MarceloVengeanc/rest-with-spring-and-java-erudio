ece21309d50c25af1893050ef8ef1828fe398b9e
mserrano <marcelo.serrano@exataid.com.br>
2024-12-13 15:56:09 -0300

    refatorar codigo

--- primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/main/java/com/br/erudio/Controllers/PersonController.java
+++ primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/main/java/com/br/erudio/Controllers/PersonController.java
@@ -4,6 +4,7 @@ import com.br.erudio.Model.Person;
 import com.br.erudio.Services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,36 +16,35 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return services.findAll();
+    }
+
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") Long id) {
         return services.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST,
+    @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) {
         return services.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT,
+    @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person) {
         return services.update(person);
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         services.delete(id);
-    }
-
-    @RequestMapping(
-            method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
-        return services.findAll();
+        return ResponseEntity.noContent().build();
     }
 
 }
--- primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/main/java/com/br/erudio/Repositories/PersonRepository.java
+++ primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/main/java/com/br/erudio/Repositories/PersonRepository.java
@@ -2,7 +2,5 @@ package com.br.erudio.Repositories;
 
 import com.br.erudio.Model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {}
