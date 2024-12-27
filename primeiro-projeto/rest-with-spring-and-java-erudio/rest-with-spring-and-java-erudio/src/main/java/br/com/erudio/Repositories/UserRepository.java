1a2dca2ca590b8e75289d46a69c79fe469d7bcf4
marcelo <marcelovserrano@outlook.com>
2024-12-27 11:41:47 -0300

    implementação do JwtTokenProvider

---
+++ primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/main/java/br/com/erudio/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.erudio.Repositories;
+
+import br.com.erudio.Data.Model.Person;
+import br.com.erudio.Data.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User WHERE u.userName=:userName")
+    User findByUserName(@Param("userName") String userName);
+
+}
--- primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/test/java/br/com/erudio/IntegrationTests/Controller/WithJson/PersonControllerJsonTest.java
+++ primeiro-projeto/rest-with-spring-and-java-erudio/rest-with-spring-and-java-erudio/src/test/java/br/com/erudio/IntegrationTests/Controller/WithJson/PersonControllerJsonTest.java
@@ -10,9 +10,7 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonMappingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
